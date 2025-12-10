d63173b3e224dc18c683fb8be0ad0530d4e7734d
wonny <212clab@gmail.com>
2025-12-10 18:17:43 +0900

    -

--- services/auth-service/src/main/java/OrangeCloud/AuthService/exception/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package OrangeCloud.AuthService.exception;
-
-public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-}
